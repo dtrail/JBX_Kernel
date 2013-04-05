e4b0ec6200df12658de654a831bb92fa67ab8db7
dtrail <mnl.manz@gmail.com>
2013-04-05 12:28:29 +0200

    Changed OPP freq numbers

--- arch/arm/mach-omap2/include/mach/emif.h
+++ arch/arm/mach-omap2/include/mach/emif.h
@@ -21,7 +21,7 @@
 #define EMIF2 1
 
 /* The maximum frequency at which the LPDDR2 interface can operate in Hz*/
-#define MAX_LPDDR2_FREQ	400000000	/* 400 MHz */
+#define MAX_LPDDR2_FREQ	500000000	/* 400 MHz */
 
 /* 19.2 MHz to be used for finding initialization values */
 #define EMIF_FREQ_19_2_MHZ 19200000	/* 19.2 MHz */
@@ -43,7 +43,7 @@
 #ifdef CONFIG_LIVE_OC
 #define EMIF_MAX_NUM_FREQUENCIES 12
 #else
-#define EMIF_MAX_NUM_FREQUENCIES 6
+#define EMIF_MAX_NUM_FREQUENCIES 8
 #endif
 /*
  * Indices into the Addressing Table array.
