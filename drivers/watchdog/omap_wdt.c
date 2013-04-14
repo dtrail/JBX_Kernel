f8aecfac7ce1bfa6981996a74d1e62f0ddd16364
dtrail <mnl.manz@gmail.com>
2013-04-14 20:37:56 +0200

    touch watchdog notifier_head cleanup

--- drivers/watchdog/omap_wdt.c
+++ drivers/watchdog/omap_wdt.c
@@ -518,12 +518,12 @@ static int __devinit omap_wdt_probe(struct platform_device *pdev)
 
 	omap_wdt_dev = pdev;
 
-	if (kernelpet && wdev->irq) {
+/*	if (kernelpet && wdev->irq) {
 		wdev->nb.notifier_call = omap_wdt_nb_func;
 		atomic_notifier_chain_register(&touch_watchdog_notifier_head,
 			&wdev->nb);
 		omap_wdt_setup(wdev);
-	}
+	} */
 
 #ifdef CONFIG_OMAP_WATCHDOG_CONTROL
 	for (i = 0; i < ARRAY_SIZE(wdt_attributes); i++) {
@@ -588,12 +588,12 @@ static int __devexit omap_wdt_remove(struct platform_device *pdev)
 	release_mem_region(res->start, resource_size(res));
 	platform_set_drvdata(pdev, NULL);
 
-	if (wdev->irq)
+/*	if (wdev->irq)
 		free_irq(wdev->irq, wdev);
 
 	if (kernelpet && wdev->irq)
 		atomic_notifier_chain_unregister(&touch_watchdog_notifier_head,
-			&wdev->nb);
+			&wdev->nb); */
 
 	iounmap(wdev->base);
 
