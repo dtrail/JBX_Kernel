af3b8167d756cea4ff49cb93fc5a4c6f29487017
dtrail <mnl.manz@gmail.com>
2013-04-14 21:04:07 +0200

    Asynchronous I/O latency to a solid-state disk greatly increased

--- fs/aio.c
+++ fs/aio.c
@@ -1620,7 +1620,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 	struct kioctx *ctx;
 	long ret = 0;
 	int i;
-	struct blk_plug plug;
+/*	struct blk_plug plug; */
 
 	if (unlikely(nr < 0))
 		return -EINVAL;
@@ -1637,7 +1637,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		return -EINVAL;
 	}
 
-	blk_start_plug(&plug);
+/*	blk_start_plug(&plug); */
 
 	/*
 	 * AKPM: should this return a partial result if some of the IOs were
@@ -1661,7 +1661,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		if (ret)
 			break;
 	}
-	blk_finish_plug(&plug);
+/*	blk_finish_plug(&plug); */
 
 	put_ioctx(ctx);
 	return i ? i : ret;
