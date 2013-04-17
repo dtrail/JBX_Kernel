ef32c878755990635c447a67ad4518eb56bd3fa2
dtrail <mnl.manz@gmail.com>
2013-04-17 17:42:58 +0200

    Changed CPU freqs for OC kernel, increased voltage limits and FIXED GPU efuse

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -622,7 +622,7 @@ static ssize_t store_gpu_oc(struct cpufreq_policy *policy, const char *buf, size
 {
 	int prev_oc, ret1, ret2; 
         struct device *dev;
-	unsigned long gpu_freqs[3] = {307200000,384000000,416000000};
+	unsigned long gpu_freqs[3] = {307200000,384000000,512000000};
 
 	prev_oc = oc_val;
 	if (prev_oc < 0 || prev_oc > 2) {
