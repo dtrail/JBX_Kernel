9097b0a3f3c98dc6f5a30f83dc9448660bad98f2
dtrail <mnl.manz@gmail.com>
2013-06-20 17:40:27 +0200

    PM: Fix a typo

--- arch/arm/mach-omap2/pm.c
+++ arch/arm/mach-omap2/pm.c
@@ -436,7 +436,7 @@ static void __init omap4_init_voltages(void)
 	}
 	omap2_set_init_voltage("core", "virt_l3_ck", l3_dev);
 	omap2_set_init_voltage("iva", "dpll_iva_m5x2_ck", iva_dev);
-   }
+   
 }
 static int __init omap2_common_pm_init(void)
 {
