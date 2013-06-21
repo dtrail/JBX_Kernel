ee5b7fc03c96bc890e42b2760b4ee2f3d42e9da4
dtrail <mnl.manz@gmail.com>
2013-06-21 12:23:28 +0200

    fixed ignore file

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 0 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#0 SMP PREEMPT Thu Jun 20 17:56:23 CEST 2013"
-#define LINUX_COMPILE_BY "jbx-kernel"
-#define LINUX_COMPILE_HOST "dtrail"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
