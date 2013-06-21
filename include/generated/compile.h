83009b0c2aa4ed91356af35e4b5bdb07507f1f26
dtrail <mnl.manz@gmail.com>
2013-06-21 12:22:17 +0200

    Fixed gitignore

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 39 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#39 SMP PREEMPT Mon Apr 1 17:15:01 CEST 2013"
-#define LINUX_COMPILE_BY "jbx-kernel"
-#define LINUX_COMPILE_HOST "dtrail"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/generated/utsrelease.h
+++
@@ -1,2 +0,0 @@
-#define KERNEL_VER_NOSCM "3.0.31"
-#define UTS_RELEASE "3.0.31"
